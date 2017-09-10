a2148c0506947bcf2eedce3b4d6a4a8ed9253c58
jeff <jeff@jamma.cn>
2017-09-10 21:13:12 +0800

    acl_user

--- src/router/workOrder.js
+++ src/router/workOrder.js
@@ -43,18 +43,6 @@ export default function (service, opts = {}) {
   service.onReady().then(() => {
     router
       .add('/', 'get', function (opts, cb, next) {
-        if (opts.headers && opts.headers.acl_user) {
-          opts.data.userId = opts.headers.acl_user
-        }
-        next()
-      })
-      .add('/', 'post', function (opts, cb, next) {
-        if (opts.headers && opts.headers.acl_user) {
-          opts.data.user = opts.headers.acl_user
-        }
-        next()
-      })
-      .add('/', 'get', function (opts, cb, next) {
         opts.conditions || (opts.conditions = {})
         if (opts.data.userId) {
           opts.conditions.user = opts.data.userId
